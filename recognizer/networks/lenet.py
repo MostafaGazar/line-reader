3d03a7a5a8bc25733192330ca903e7a9694934ea
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-19 17:03:18 +0000

    Try a more complex lenet network

--- recognizer/networks/lenet.py
+++ recognizer/networks/lenet.py
@@ -8,16 +8,28 @@ from tensorflow.python.keras import layers
 def lenet5(input_shape: Shape, number_of_classes: int) -> Model:
     model = Sequential()
 
-    model.add(layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=input_shape))  # (32, 32, 1)
-    model.add(layers.AveragePooling2D())
+#     model.add(layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=input_shape))  # (32, 32, 1)
+#     model.add(layers.AveragePooling2D())
 
-    model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
-    model.add(layers.AveragePooling2D())
+#     model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
+#     model.add(layers.AveragePooling2D())
 
+#     model.add(layers.Flatten())
+
+#     model.add(layers.Dense(units=120, activation='relu'))
+#     model.add(layers.Dense(units=84, activation='relu'))
+#     model.add(layers.Dense(units=number_of_classes, activation='softmax'))
+
+    model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(0.2))
+    
     model.add(layers.Flatten())
-
-    model.add(layers.Dense(units=120, activation='relu'))
-    model.add(layers.Dense(units=84, activation='relu'))
-    model.add(layers.Dense(units=number_of_classes, activation='softmax'))
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.2))
+    
+    model.add(layers.Dense(number_of_classes, activation='softmax'))
 
     return model
