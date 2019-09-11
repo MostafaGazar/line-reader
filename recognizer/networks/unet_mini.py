cef38b7f202fe0bedb165aa6a4398a4184514d97
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-11 00:32:13 +0000

    Clean lines segmentation notebooks

--- recognizer/networks/unet_mini.py
+++ recognizer/networks/unet_mini.py
@@ -9,26 +9,31 @@ def unet_mini(network_input: NetworkInput) -> KerasModel:
     inputs = Input(network_input.input_shape)
 
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = BatchNormalization()(conv1)
     conv1 = Dropout(0.2)(conv1)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
     pool1 = MaxPooling2D((2, 2))(conv1)
 
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = BatchNormalization()(conv2)
     conv2 = Dropout(0.2)(conv2)
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
     pool2 = MaxPooling2D((2, 2))(conv2)
 
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = BatchNormalization()(conv3)
     conv3 = Dropout(0.2)(conv3)
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
 
     up1 = concatenate([UpSampling2D((2, 2))(conv3), conv2], axis=-1)
     conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1)
+    conv4 = BatchNormalization()(conv4)
     conv4 = Dropout(0.2)(conv4)
     conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
 
     up2 = concatenate([UpSampling2D((2, 2))(conv4), conv1], axis=-1)
     conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(up2)
+    conv5 = BatchNormalization()(conv5)
     conv5 = Dropout(0.2)(conv5)
     conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv5)
 
